5b7a7cf9a1cf22a77fec178dde83044bee60da45
Benerous <Benerous@users.noreply.github.com>
2020-03-17 14:24:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Addition from './components/Addition';
+import Substruction from './components/Substraction';
+import Multiply from './components/Multiply';
+import Division from './components/Division';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    step: "start"
+  };
+
+  selectStep = (e) => {
+    this.setState({
+      step: e.target.value
+    });
+  };
+
+  render() {
+    switch (this.state.step) {
+      case "start":
+          return (
+            <div className="container">
+              <div className="text-center">
+                <button 
+                  onClick={this.selectStep} 
+                  value="start" 
+                  className="btn btn-lg btn-outline-primary my-5 w-50">
+                  Math trainer
+                </button>
+                <br />
+                <button 
+                  className="btn btn-outline-dark mx-auto my-2 w-25" 
+                  onClick={this.selectStep} 
+                  value="addition">
+                  Addition
+                </button>
+                <br />
+                <button 
+                  className="btn btn-outline-dark mx-auto my-2 w-25" 
+                  onClick={this.selectStep} 
+                  value="substraction">
+                  Substraction
+                </button>
+                <br />
+                <button 
+                  className="btn btn-outline-dark mx-auto my-2 w-25" 
+                  onClick={this.selectStep} 
+                  value="multiply">
+                  Multiply
+                </button>
+                <br />
+                <button 
+                  className="btn btn-outline-dark mx-auto my-2 w-25" 
+                  onClick={this.selectStep} 
+                  value="division">
+                  Division
+                </button>
+                <br />
+              </div>
+            </div>
+          );
+      case "addition":
+          return (
+            <Addition 
+              selectStep={this.selectStep}
+            />
+          )
+      case "substraction":
+          return (
+            <Substruction 
+              selectStep={this.selectStep}
+            />
+          )
+      case "multiply":
+          return (
+            <Multiply 
+              selectStep={this.selectStep}
+            />
+          )
+      case "division":
+          return (
+            <Division 
+              selectStep={this.selectStep}
+            />
+          )
+      default:
+          return <h1>Success</h1>;
+    };
+  };
+};
 
 export default App;
---
+++ src/components/Division.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export default class Division extends Component {
+    render() {
+        const { selectStep } = this.props;
+        return (
+            <div className="container text-center">
+                <button 
+                    onClick={selectStep} 
+                    value="start" 
+                    className="btn btn-lg btn-outline-primary my-5 w-50">
+                    Math trainer
+                </button>
+            </div>
+        )
+    }
+}
